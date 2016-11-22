7ea900a28c66c27fa0c3f5085090de5894faa62f
DavidGL17 <david.gonzalezleon@epfl.ch>
2016-11-22 21:45:37 +0100

    Set sprite

--- src/platform/game/Actor.java
+++ src/platform/game/Actor.java
@@ -14,13 +14,11 @@ public abstract class Actor implements Comparable<Actor> {
 	private int priority;
 	private World world;
 	private Sprite sprite;
-	private Loader loader;
 
 	//Pour fireball et player
 	public Actor(int p,Loader loader,String dessin){
 		priority=p;
 		sprite = loader.getSprite(dessin);
-		this.loader=loader;
 	}
 	//Pour les blocks
 	public Actor(int p,Sprite sprite){
@@ -29,6 +27,29 @@ public abstract class Actor implements Comparable<Actor> {
 	}
 	//Pour les levels
 	public Actor(){
+	}
+	
+	//Getters and setters
+	protected World getWorld(){
+		return world;
+	}
+	public Sprite getSprite(){
+		return sprite;
+	}
+	public Box getBox () {
+		return null;
+	}
+	public Vector getPosition () {
+		Box box = getBox() ;
+		if (box == null)
+			return null ;
+			return box.getCenter () ;
+	}
+	public int getPriority(){
+		return priority;
+	}
+	protected void setSprite(String dessin){
+		sprite = world.getLoader().getSprite(dessin);
 	}
 	
 	// pour évoluer au cours du temps :
@@ -47,21 +68,9 @@ public abstract class Actor implements Comparable<Actor> {
 			}
 		}
 	}
-	public int getPriority(){
-		return priority;
-	}
 	public void interact(Actor other) {}
 	public boolean isSolid () {
 		return false ;
-	}
-	public Box getBox () {
-		return null;
-	}
-	public Vector getPosition () {
-		Box box = getBox() ;
-		if (box == null)
-			return null ;
-			return box.getCenter () ;
 	}
 	public void preUpdate(){}
 	public void postUpdate(){}
@@ -70,12 +79,6 @@ public abstract class Actor implements Comparable<Actor> {
 	}
 	public void unregister () {
 		world = null ;
-	}
-	protected World getWorld(){
-		return world;
-	}
-	public Sprite getSprite(){
-		return sprite;
 	}
 	public boolean hurt(Actor instigator , Damage type , double amount , Vector location) {
 		return false ;
