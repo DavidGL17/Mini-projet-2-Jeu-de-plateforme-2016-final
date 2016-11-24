4631cf6fc0806f05ba85b8cd3af690f79fcd6553
TozeMarques <toze_maluko@hotmail.com>
2016-11-24 13:16:30 +0100

    Spikes

---
+++ src/platform/game/Spikes.java
@@ -0,0 +1,39 @@
+package platform.game;
+
+import platform.util.Box;
+import platform.util.Input;
+import platform.util.Vector;
+import platform.util.Output;
+import platform.util.Sprite;
+import platform.util.Loader;
+
+public class Spikes extends Actor{
+
+	private Vector position;
+	private final static String spikeHaut = "spikehaut";
+	private final static String spikeBas = "spikebas";
+	private final static String spikeGauche = "spikegauche";
+	private final static String spikeDroite = "spikedroite";
+	
+	public Spikes(Vector position, Loader loader){
+		super(1400, new Box(position, 1, 1), loader, spikeHaut);
+		this.position = position;
+		
+	}
+	
+	public void interact(Actor other) {
+		super.interact(other);
+		if (getBox().isColliding(other.getBox())){
+			other.hurt(this , Damage.PHYSICAL , 2.0 ,Vector.ZERO);
+		}
+	}
+	
+	public boolean isSolid(){
+		return true;
+	}
+	public void draw(Input input, Output output){
+		output.drawSprite(getSprite(), getBox());
+	}
+	
+	
+}
