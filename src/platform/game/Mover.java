76078e9fe5803054ab55af160e689c5fd3a61129
DavidGL17 <david.gonzalezleon@epfl.ch>
2016-11-25 21:16:44 +0100

    Mover pas encore au point

--- src/platform/game/Mover.java
+++ src/platform/game/Mover.java
@@ -1,17 +1,20 @@
 package platform.game;
 
 import platform.util.Box;
+import platform.util.Input;
 import platform.util.Loader;
 import platform.util.Vector;
 
 public class Mover extends Block{
 	private Vector off;
 	private Vector  on;
+	private Vector currentPosition;
 	private Box offBox;
 	private Box onBox;
 	private Signal signal;
 	private static double Width;
 	private static double Height;
+	private double current = 0;
 	
 	public Mover (Vector off, Vector on, double Width, double Height,Loader loader,Signal signal, String dessin){
 		super(new Box(off, Width, Height), loader.getSprite(dessin));
@@ -22,12 +25,30 @@ public class Mover extends Block{
 		onBox = new Box(on, Width, Height);
 		this.Width = Width;
 		this.Height = Height;
+		currentPosition = off;
 	}
 	public Box getBox(){
-		if (signal.isActive()){
-			return onBox;
-		} else {
-			return offBox;
+		return new Box(currentPosition, Width, Height);
+	}
+
+	@Override
+	public void update(Input input) {
+		super.update(input) ;
+		if (signal.isActive()) {
+			current += input.getDeltaTime () ;
+			if (current > 1.0){
+				current = 1.0 ;
+				currentPosition=on;
+			}
+			currentPosition = new Vector(off.getX()-((current*(off.getX()-on.getX()))), off.getY()-((current*(off.getY()-on.getY()))));
+		}
+		if (!signal.isActive()){
+			current -= input.getDeltaTime ();
+			if (current < 0.0){
+				current = 0.0 ;
+				currentPosition = off;
+			}
+			currentPosition = new Vector(on.getX()-(1/(current*(on.getX()-off.getX()))), on.getY()-(1/(current*(on.getY()-off.getY()))));
 		}
 	}
 }
