e22dd4ee448c8784a6faa0f53070753d3a53a339
DavidGL17 <david.gonzalezleon@epfl.ch>
2016-11-25 08:27:42 +0100

    Locker

---
+++ src/platform/game/Locker.java
@@ -0,0 +1,42 @@
+package platform.game;
+
+import platform.util.Box;
+import platform.util.Input;
+import platform.util.Loader;
+import platform.util.Output;
+import platform.util.Vector;
+
+public class Locker extends Block implements Signal{
+	private Signal signal;
+	private Vector position;
+	private final static double SIZE =1;
+	private final static String blue = "lock.blue";
+	private final static String red = "lock.red";
+	private final static String yellow = "lock.yellow";
+	private final static String green = "lock.green";
+
+	
+	public Locker(Vector position, Loader loader, String color,Signal signal){
+		super(new Box(position, SIZE, SIZE), loader.getSprite(color));
+		this.signal= signal;
+		this.position=position;
+	}
+	
+	// pour évoluer au cours du temps :
+	public void update(Input input) {
+		if (signal.isActive()){
+			setBox(null);
+		}
+	}
+	// pour être dessiné
+	public void draw(Input input , Output output) {
+		if (!signal.isActive()){
+			output.drawSprite(getSprite(), getBox());
+		}
+	}
+
+	@Override
+	public boolean isActive() {
+		return signal.isActive();
+	}
+}
