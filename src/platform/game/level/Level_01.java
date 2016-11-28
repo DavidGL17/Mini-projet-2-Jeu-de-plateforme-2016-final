b95d7659f128c9eb888a838fbe256767e4cd4713
TozeMarques <toze_maluko@hotmail.com>
2016-11-28 12:54:22 +0100

    2 new blocks

--- src/platform/game/level/Level_01.java
+++ src/platform/game/level/Level_01.java
@@ -1,5 +1,30 @@
 package platform.game.level;
 
+import platform.game.Player;
+import platform.game.World;
+import platform.util.Vector;
+
 public class Level_01 extends Level {
 
+	 @Override
+	    public void register(World world) {
+	        super.register(world);
+	        
+	        // Register a new instance, to restart level automatically
+	        world.setNextLevel(new BasicLevel());
+	    	Player franky = new Player(new Vector(0, -4), new Vector(-10, 12), world.getLoader());
+	    	world.register(franky);
+	        
+	        
+	        
+	        
+	 }
+	
+	
+	
+	
+	
+	
+	
+	
 }
