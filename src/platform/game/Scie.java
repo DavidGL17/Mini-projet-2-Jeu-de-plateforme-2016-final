ca4d285f2a8fcd3f14c23659a2ff6b1de61f83a2
DavidGL17 <david.gonzalezleon@epfl.ch>
2016-11-29 08:13:04 +0100

    Scie

---
+++ src/platform/game/Scie.java
@@ -0,0 +1,26 @@
+package platform.game;
+
+import platform.util.Box;
+import platform.util.Input;
+import platform.util.Loader;
+import platform.util.Output;
+import platform.util.Vector;
+
+public class Scie extends Actor{
+	
+	public Scie(Box box, Loader loader, String dessin){
+		super(8000, box, loader, dessin);
+	}
+	
+	public boolean isSolid(){
+		return true;
+	}
+	public void interact(Actor other){
+		if (other.getBox().isColliding(getBox())){
+			other.hurt(this, Damage.VOID, Damage.VOID.getDamage(), getPosition());
+		}
+	}
+	public void draw(Input input, Output output){
+		output.drawSprite(getSprite(), getBox(), input.getTime()*4);
+	}
+}
