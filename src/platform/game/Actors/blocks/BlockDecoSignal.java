cbe1f5efd8c32d2e3adaa23844a798c5972c9334
DavidGL17 <david.gonzalezleon@epfl.ch>
2016-12-06 07:46:38 +0100

    Block deco signal

---
+++ src/platform/game/Actors/blocks/BlockDecoSignal.java
@@ -0,0 +1,22 @@
+package platform.game.Actors.blocks;
+
+import platform.game.Signals.Signal;
+import platform.util.Input;
+import platform.util.Loader;
+import platform.util.Output;
+import platform.util.Vector;
+
+//Nous utilisons cette classe pour siplay du texte principalement
+public class BlockDecoSignal extends BlockDeco{
+	private final Signal signal;
+	
+	public BlockDecoSignal(Vector position, double height, double width, Loader loader,String dessin,Signal signal){
+		super(position, height, width, loader, dessin);
+		this.signal = signal;
+	}
+	public void draw (Input input, Output output){
+		if (signal.isActive()){
+			output.drawSprite(getSprite(), getBox());
+		}
+	}
+}
