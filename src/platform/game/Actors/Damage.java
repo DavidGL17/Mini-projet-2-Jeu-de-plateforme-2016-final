74602a1cc561a00b5cf6b8e074b2d684ab53e190
TozeMarques <toze_maluko@hotmail.com>
2016-12-09 13:19:17 +0100

    Damage change

--- src/platform/game/Actors/Damage.java
+++ src/platform/game/Actors/Damage.java
@@ -1,12 +1,12 @@
 package platform.game.Actors;
 
 public enum Damage {
-FIRE (2.0),
-PHYSICAL (2),
+FIRE (1.0),
+PHYSICAL (1),
 AIR (10),VOID (1000000),
 ACTIVATION (0.1),
 HEAL (2),
-SMALLMONSTER (3);
+SMALLMONSTER (2);
 	
 	private double damage = 0.0;
 	
