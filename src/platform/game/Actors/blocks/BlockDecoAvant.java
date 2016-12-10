d796fe3acc5fab224310859886c6174aa3bb79d5
DavidGL17 <david.gonzalezleon@epfl.ch>
2016-12-10 16:38:15 +0100

    Block deco Avant opti

--- src/platform/game/Actors/blocks/BlockDecoAvant.java
+++ src/platform/game/Actors/blocks/BlockDecoAvant.java
@@ -1,30 +1,12 @@
 package platform.game.Actors.blocks;
 
-import platform.util.Box;
-import platform.util.Input;
 import platform.util.Loader;
-import platform.util.Output;
 import platform.util.Vector;
 
 public class BlockDecoAvant extends BlockDeco {
-	private Vector position;
-	private final double WIDTH;
-	private final double HEIGHT;
 	
 	public BlockDecoAvant(Vector position, double height, double width, Loader loader, String dessin) {
 		super(position, height, width, loader, dessin);
-		WIDTH = width;
-		HEIGHT = height;
-		this.position = position;
 		this.setPriority(10000);
-		
-		
-	}
-	public boolean isSolid(){
-		return false;
-	}
-	public void draw(Input input, Output output){
-		output.drawSprite(getSprite(), new Box(position, WIDTH, HEIGHT));	
-		
 	}
 }
