77f2ac6bd7ecd8a1d7ef9fbd5537e41470686113
DavidGL17 <david.gonzalezleon@epfl.ch>
2016-12-11 10:58:18 +0100

    Level Intro decco et début affiche

--- src/platform/game/Actors/levels/LevelIntro.java
+++ src/platform/game/Actors/levels/LevelIntro.java
@@ -1,6 +1,7 @@
 package platform.game.Actors.levels;
 
 import platform.game.Actors.Exit;
+import platform.game.Actors.LimiteTangible;
 import platform.game.Actors.Player;
 import platform.game.Actors.World;
 import platform.game.Actors.blocks.Block;
@@ -15,12 +16,25 @@ public class LevelIntro extends Level{
     public void register(World world) {
         super.register(world);
         Loader loader = getWorld().getLoader();
-        Player franky = new Player(new Vector(0, 0), new Vector(0, 0), world.getLoader());
+        Player franky = new Player(new Vector(0, 0), new Vector(0, -20), world.getLoader());
         world.register(franky);
-        world.register(new Block(new Box(new Vector(0, -30), 12, 2), loader.getSprite("stone.3")));
+        world.register(new LimiteTangible(new Vector(-6.5, 0), 1, 100));
+        world.register(new LimiteTangible(new Vector(6.5, 0), 1, 100));
+        
+        //portes
         world.register(new Exit(new Vector(5, -28), 1.5, 2, loader, new Constant(true),1));
-        world.register(new Exit(new Vector(-5, -28), 1.5, 2, loader, new Constant(true),2));
         world.register(new BlockDeco(new Vector(5, -26), 3, 5, loader, "zoneTexteLevelintro_1"));
+        world.register(new Exit(new Vector(-5, -28), 1.5, 2, loader, new Constant(true),2));
         world.register(new BlockDeco(new Vector(-5, -26), 3, 5, loader, "zoneTexteLevelintro_2"));
+        
+        //platforme
+        for (int i = 0;i<7;++i){
+        	world.register(new Block(new Box(new Vector(-18+(6*i), -32), 6, 6),loader.getSprite("grass.middle.center3")));
+        	world.register(new Block(new Box(new Vector(-18+(6*i), -38), 6, 6),loader.getSprite("grass.center3")));
+        }
+        //background
+	    world.register(new BlockDeco(new Vector(0, -22), 15, 25, world.getLoader(), "Background_18"));
+	    world.register(new BlockDeco(new Vector(0, 0), 500, 505, world.getLoader(), "Background2_1"));
+
 	}
 }
