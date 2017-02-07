1ee6fc50235584234bf1d5ebcd4e942ffa6e71c7
DavidGL17 <david.gonzalezleon@epfl.ch>
2017-02-07 18:18:25 +0100

    small fixes

--- src/platform/game/Actors/levels/Level_Matrices.java
+++ src/platform/game/Actors/levels/Level_Matrices.java
@@ -1,26 +1,15 @@
 package platform.game.Actors.levels;
 
 import platform.game.World;
-import platform.game.Actors.Locker;
-import platform.game.Actors.actorSignal.SignalSouris;
-import platform.game.Actors.blocks.BlockDeco;
-import platform.util.Box;
 import platform.util.Loader;
-import platform.util.Vector;
 
 public class Level_Matrices extends Level{
-	
+
 	@Override
     public void register(World world) {
     	super.register(world);
     	world.thereIsNoPlayer();
-    	
     	Loader loader = world.getLoader();
     	
-    	
-    	SignalSouris signal = new SignalSouris(new Box(new Vector(0, 0), 3, 3),true);
-    	world.register(signal);
-	    world.register(new Locker(new Vector(0, 5), loader, Locker.blue, signal));
-	    world.register(new BlockDeco(new Vector(0, 0), 3, 3, loader, "portal1"));
     }
 }
